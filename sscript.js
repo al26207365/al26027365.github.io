875518d39061f8029e298673b7a2eae5807cf73c
al26207365 <112560887+al26207365@users.noreply.github.com>
2022-10-29 00:47:37 +0800

    Add files via upload

---
+++ sscript.js
@@ -0,0 +1,33 @@
+//註解
+// "字串用法"
+  
+
+//var h1 = document.getElmentByid("header");
+var entry_count = 0;
+//var entry_limit = 3;
+//var out_of_limit = false;
+var password = 26027365;
+var input;
+  
+///h1.innerText = "小鍾鍾"
+//h1.style.backgroundColor = "red";
+//h1.style.color = "blue"
+
+
+while(password!=input){
+    entry_count ++;
+    if (entry_count){
+    input=prompt("請輸入密碼")
+    
+
+}
+    else {
+        alert("登入成功")
+    }
+}    
+
+
+
+
+//document.write("<h1>小鍾的網頁<\h1>");
+
---
+++ sscript2.js
@@ -0,0 +1,1 @@
+document.write("<h1>script2 test<\h1>");
